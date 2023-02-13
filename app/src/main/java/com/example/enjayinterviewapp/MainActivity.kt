4b7d6c4ce8655272e136570aa3692aac9a185e81
Shahil <200233043016@silveroakuni.ac.in>
2023-02-13 11:17:21 +0530

    login successful

--- app/src/main/java/com/example/enjayinterviewapp/MainActivity.kt
+++ app/src/main/java/com/example/enjayinterviewapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.enjayinterviewapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@MainActivity,login::class.java))
+                finish()
+            },1000)
+
     }
 }
---
+++ app/src/main/java/com/example/enjayinterviewapp/User.kt
@@ -0,0 +1,7 @@
+package com.example.enjayinterviewapp
+
+data class User(
+    val FName:String? =null, val Mobile: String? =null, val Email: String? =null, val Date:String?=null, val Password:String?=null)
+{
+
+}
